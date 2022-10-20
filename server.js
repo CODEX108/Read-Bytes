dd478f553f026ab8366e93033fee6252acc07edf
Manjiri Milind Chavande <cmanjiri1912@gmail.com>
2022-10-20 18:15:12 +0530

    initial author routes

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
-// hook the index file from router dir
+const bodyParser = require('body-parser')
+// hook the index file from routes dir
 const indexRouter = require('./routes/index')
-
+const authorRouter = require('./routes/authors')
 
 
 
@@ -21,12 +22,14 @@ app.set('layout','layouts/layout')
 app.use(expressLayouts)
 // tell where our public files are
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit:'10mb',extended:false}))
+
 
 
 
 // Note the way you place your code does matter here,see to it that this block of code is after the initialization of the express modules
 const mongoose = require('mongoose')
-const { use } = require('./routes/index')
+// const { use } = require('./routes/index')
 mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true})
 const db= mongoose.connection
 db.on('error',error=>console.error(error))
@@ -34,7 +37,7 @@ db.once('open',()=>console.log('connected to mongoose'))
 
 
 app.use('/',indexRouter)
+app.use('/authors',authorRouter)
 
 // pull from the env variable 
-app.listen(process.env.PORT || 3000)
-
+ app.listen(process.env.PORT || 3000)
